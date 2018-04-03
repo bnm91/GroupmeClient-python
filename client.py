ced676be6c61e8c220b2b6508e785006df9a4667
bnm91 <ball.nixonm@gmail.com>
2018-04-02 22:01:25 -0700

    first pass at makeCall method

--- client.py
+++ client.py
@@ -1,16 +1,34 @@
 from ApiWrapper import groups
 from ApiWrapper import members
 from ApiWrapper import messages
+from ApiWrapper import users
 
 
 
 
 
 def Client(object):
-    accessToken = ''
 
     def __init__(self, groupmeAccessToken):
-        accessToken = groupmeAccessToken
+        self.accessToken = groupmeAccessToken
+        self.groups = None
+        self.Users = None
+        self.messages = None
+        self.members = None
+
+    def makeCall(self, groupmeObject, call, **kwargs):
+        if  groupmeObject == 'users':
+            if self.Users is None:
+                self.Users = users.Users(self.accessToken)
+            if call == 'get':
+                self.Users.get()
+        
+        if groupmeObject == 'groups':
+            if self.groups is None:
+                self.groups = groups.Groups(self.accessToken)
+            getattr(self.groups, call)(**kwargs)
+
+
 
 
 
@@ -29,7 +47,7 @@ def Client(object):
 
 
 
-messagesInstance = messages.Messages(39628471)
+# messagesInstance = messages.Messages(39628471)
 # print(messagesInstance.get())
 # print(messagesInstance.get())
 # print(messagesInstance.unlike(152248319326115503))
