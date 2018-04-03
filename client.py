0f00270cf9cfb138cffd09683126908fe5b7e376
bnm91 <ball.nixonm@gmail.com>
2018-04-02 22:57:52 -0700

    adding help text to makeCall and having it return the response

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ from ApiWrapper import groups
 from ApiWrapper import members
 from ApiWrapper import messages
 from ApiWrapper import users
+# from config import config
 
 
 
@@ -17,28 +18,53 @@ class Client(object):
         self.members = None
 
     def makeCall(self, groupmeObject, call, **kwargs):
+        '''
+        Currently implemented calls, by groupmeObject\n
+        \n
+        users
+            get
+        groups
+            getGroups
+            getFormer
+            getGroup
+            createGroup
+            updateGroup
+            destroyGroup
+        members
+            add
+            remove
+            update
+        messages
+            get
+            create
+            like
+
+        '''
+
         if  groupmeObject == 'users':
             if self.Users is None:
                 self.Users = users.Users(self.accessToken)
             if call == 'get':
-                self.Users.get()
+                return self.Users.get()
         
         if groupmeObject == 'groups':
             if self.groups is None:
                 self.groups = groups.Groups(self.accessToken)
-            getattr(self.groups, call)(**kwargs)
+            return getattr(self.groups, call)(**kwargs)
 
         if groupmeObject == 'members':
             if self.members is None:
                 if kwargs.items()['groupId'] is not None:
                     self.members = members.Members(self.accessToken, kwargs.items()['groupId'])
-            getattr(self.members, call)(**kwargs)
+            return getattr(self.members, call)(**kwargs)
             
         if groupmeObject == 'messages':
             if self.messages is None:
                 if kwargs.items()['groupId'] is not None:
                     self.messages = messages.Messages(self.accessToken, kwargs.items()['groupId'])
-            getattr(self.messages, call)(**kwargs)
+            return getattr(self.messages, call)(**kwargs)
+        
+        return 'error'
             
 
 
@@ -50,18 +76,7 @@ class Client(object):
 
 
 
+# # help(groups.getGroups())
+# client = Client(config['accessToken'])
+# print(client.makeCall('groups', 'createGroup', name='Client Test Group'))
 
-
-
-
-# print(messages.crosspackagetest())
-
-# help(groups.getGroups())
-# print(groups.getGroups())
-
-
-
-# messagesInstance = messages.Messages(39628471)
-# print(messagesInstance.get())
-# print(messagesInstance.get())
-# print(messagesInstance.unlike(152248319326115503))
