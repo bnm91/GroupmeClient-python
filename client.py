458a3cc3fcc60a27296ddb2eb2f55a6dfdecfa26
bnm91 <ball.nixonm@gmail.com>
2018-04-02 21:46:49 -0700

    renaming app to client

---
+++ client.py
@@ -0,0 +1,35 @@
+from ApiWrapper import groups
+from ApiWrapper import members
+from ApiWrapper import messages
+
+
+
+
+
+def Client(object):
+    accessToken = ''
+
+    def __init__(self, groupmeAccessToken):
+        accessToken = groupmeAccessToken
+
+
+
+
+
+
+
+
+
+
+
+# print(messages.crosspackagetest())
+
+# help(groups.getGroups())
+# print(groups.getGroups())
+
+
+
+messagesInstance = messages.Messages(39628471)
+# print(messagesInstance.get())
+# print(messagesInstance.get())
+# print(messagesInstance.unlike(152248319326115503))
