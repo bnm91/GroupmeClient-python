933778d3cb6e9154aa473bfc8c3f558fd86f3fcf
Nixon Ball <ball.nixonm@gmail.com>
2018-04-15 17:38:17 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'GroupmeClient',
+  packages = ['GroupmeClient'],
+  version = '0.1',
+  description = 'A python library for using GroupMe's API',
+  author = 'Nixon Ball',
+  author_email = 'ball.nixonm@gmail.com',
+  url = 'https://github.com/bnm91/GroupmeClient-python', 
+  download_url = 'https://github.com/bnm91/GroupmeClient-python/archive/master.zip', 
+  keywords = ['GroupMe', 'API', 'client', 'python'], 
+  classifiers = [],
+)
